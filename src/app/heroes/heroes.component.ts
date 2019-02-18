d9ee64e83dfb827dc70b1ec28c423857e6fd3ba7
ravindra.gaikawad <ravindra.gaikwad@mobiliya.com>
2019-02-18 15:04:16 +0530

    Final code review - After HTTP

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,9 +17,9 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  delete(hero: Hero): void {
-    this.heroes = this.heroes.filter(h => h !== hero);
-    this.heroService.deleteHero(hero).subscribe();
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   add(name: string): void {
@@ -31,8 +31,9 @@ export class HeroesComponent implements OnInit {
       });
   }
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
+
 }
