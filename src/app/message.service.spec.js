f5f2db82e0b255c9122532c21cbd24b7304e5138
ravindra.gaikawad <ravindra.gaikwad@mobiliya.com>
2019-02-18 12:01:03 +0530

    MessageService for loosely-coupled communication between classes

---
+++ src/app/message.service.spec.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var testing_1 = require("@angular/core/testing");
+var message_service_1 = require("./message.service");
+describe('MessageService', function () {
+    beforeEach(function () { return testing_1.TestBed.configureTestingModule({}); });
+    it('should be created', function () {
+        var service = testing_1.TestBed.get(message_service_1.MessageService);
+        expect(service).toBeTruthy();
+    });
+});
+//# sourceMappingURL=message.service.spec.js.map
